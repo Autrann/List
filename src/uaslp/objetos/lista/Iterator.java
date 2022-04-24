6997689cbfc24469104df2721ac6449897d110ca
Autrann <luis.autran2@gmail.com>
2022-04-24 18:31:28 -0500

    Convert to Maven

--- src/uaslp/objetos/lista/Iterator.java
+++
@@ -1,7 +0,0 @@
-package uaslp.objetos.lista;
-
-public interface Iterator <T>{
-    boolean hasNext();
-
-    T next();
-}
--- src/uaslp/objetos/lista/exception/NotNullValuesAllowedException.java
+++
@@ -1,8 +0,0 @@
-package uaslp.objetos.lista.exception;
-
-public class NotNullValuesAllowedException  extends Exception{
-
-    public NotNullValuesAllowedException() {
-        super("Null values are not allowed");
-    }
-}
